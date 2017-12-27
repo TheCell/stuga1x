b15549782af5b6681bc27c9209faf26d66df6ea4
Simon <simon_hischier@hotmail.com>
2017-12-27 15:45:13 +0100

    added gui.js and uniform color attribute

---
+++ webgl/js/options.js
@@ -0,0 +1,44 @@
+let gui;
+let options =
+{
+	teamcolor1: "#ff00ff",
+	teamcolor2: "#49CC36"
+}
+
+
+window.onload = function()
+{
+	gui = new dat.GUI();
+	// add saving
+	gui.remember(options);
+
+	let colorFolder = gui.addFolder("Teamcolors");
+	let teamcolor1 = colorFolder.addColor(options, "teamcolor1");
+	let teamcolor2 = colorFolder.addColor(options, "teamcolor2");
+	colorFolder.open();
+
+	// add events
+	teamcolor1.onChange( function (value)
+	{
+		objects.forEach(function (element, index, arr)
+		{
+			if (element.material.type == "ShaderMaterial"
+				&& element.teamNumber == 1)
+			{
+				element.material.uniforms.teamcolor.value = new THREE.Color(options.teamcolor1);
+			}
+		});
+	});
+
+	teamcolor2.onChange( function (value)
+	{
+		objects.forEach(function (element, index, arr)
+		{
+			if (element.material.type == "ShaderMaterial"
+				&& element.teamNumber == 2)
+			{
+				element.material.uniforms.teamcolor.value = new THREE.Color(options.teamcolor2);
+			}
+		});
+	});
+};
