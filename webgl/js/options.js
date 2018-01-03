a543761f5b083b748b62e7433f1f2ae8c634d5ef
Simon <simon_hischier@hotmail.com>
2018-01-03 15:20:42 +0100

    added beam as option

--- webgl/js/options.js
+++ webgl/js/options.js
@@ -2,9 +2,34 @@ let gui;
 let options =
 {
 	teamcolor1: "#ff9500",
-	teamcolor2: "#49CC36"
+	teamcolor2: "#49CC36",
+	// beamOffTeam: {},
+	beamOffTime: 1000,
+	beamOffTeam1: function ()
+	{
+		for (let i = 0; i < objects.length; i++)
+		{
+			if (objects[i].teamNumber == 1)
+			{
+				console.log("iteration nr " + i);
+				startBeam( objects[i], options.beamOffTime );
+				return;
+			}
+		}
+	},
+	beamOffTeam2: function ()
+	{
+		for (let i = 0; i < objects.length; i++)
+		{
+			if (objects[i].teamNumber == 2)
+			{
+				console.log("iteration nr " + i);
+				startBeam( objects[i], options.beamOffTime );
+				return;
+			}
+		}
+	}
 }
-
 
 window.onload = function()
 {
@@ -13,9 +38,16 @@ window.onload = function()
 	gui.remember(options);
 
 	let colorFolder = gui.addFolder("Teamcolors");
+	let animationsFolder = gui.addFolder("Animations");
 	let teamcolor1 = colorFolder.addColor(options, "teamcolor1");
 	let teamcolor2 = colorFolder.addColor(options, "teamcolor2");
+	// let beamOffTeam = animationsFolder.add(options, "beamOffTeam", {team1: 0, team2: 1});
+	animationsFolder.add(options, "beamOffTeam1");
+	animationsFolder.add(options, "beamOffTeam2");
+	animationsFolder.add(options, "beamOffTime").min(10).max(5000);
+
 	colorFolder.open();
+	animationsFolder.open();
 
 	// add events
 	teamcolor1.onChange( function (value)
